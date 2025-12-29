45d664e6ea1f000bfa7a725bf83d08fcb52202de
Mayank gupta <nisebhayop.69@gmail.com>
2025-12-30 04:56:47 +0530

    music app

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,13 @@
+import { MeteorsDemo } from "@/src/components/MeteorsDemo";
+function page(){
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <div className="w-full max-w-xl">
+        <MeteorsDemo />
+      </div>
+    </div>
+  );
+}
+
+
+export default page
